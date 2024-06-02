063f1a74d31e9fff2cd3d0bd56b5fc86b62b2d8d
WayDansten <way.dansten@gmail.com>
2024-06-02 13:04:29 +0300

    Bugfixes

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import utility.management.ClientModule;
+
+public class Main {
+    public static void main(String[] args) {
+        ClientModule clientModule = new ClientModule();
+        clientModule.launch();
+    }
+}
